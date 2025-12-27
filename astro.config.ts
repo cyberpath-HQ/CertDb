c76de7c59d38ae90a1c3e29284a0b10e618824bf
Ebalo <12586380+ebalo55@users.noreply.github.com>
2025-12-27 10:01:57 +0100

    Update Astro configuration with base and trailingSlash
    Signed-off-by: Ebalo <12586380+ebalo55@users.noreply.github.com>

--- astro.config.ts
+++ astro.config.ts
@@ -13,6 +13,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site:         "https://certdb.cyberpath-hq.com",
+    base:          `/`,
+    trailingSlash: `ignore`,
     integrations: [
         tailwind({
             nesting: true,
